dd66c258752de00d0987306c5a51c2c5d85ab737
vuanphu <48764733+vuanphu@users.noreply.github.com>
2019-04-11 21:56:37 +0700

    homecontroller

---
+++ ttn/WebBanDT/WebBanDT/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models.Data;
+
+namespace WebBanDT.Controllers
+{
+    public class HomeController : Controller
+    {
+        BanDT1 db = new BanDT1();
+        // GET: Home
+        public ActionResult Index(int? pageTemp)
+        {
+            int pageSize = 5;
+            int pageNumber = pageTemp ?? 1;
+            return View(db.Phones.ToList().OrderBy(a => a.Id).ToPagedList(pageNumber, pageSize));
+            
+        }
+    }
+}
