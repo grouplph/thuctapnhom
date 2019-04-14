63f2dce73e33739985326504136197971a52857e
hieptran98 <hiephaha98@gmail.comm>
2019-04-14 09:09:19 +0700

    Create GiatangdanController.cs

---
+++ ttn/WebBanDT/WebBanDT/Controllers/GiatangdanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models;
+
+namespace WebBanDT.App_Start.Controllers
+{
+    public class GiatangdanController : Controller
+    {
+        
+        // GET: Giatangdan
+        public ActionResult Index()
+        {
+            Giatangdan strHome = new Giatangdan();
+            List<Giatangdan> obj = strHome.GetHome();
+            return View(obj);
+        }
+    }
+}
