b290154b2d2102821bffbe82a56c977636181f48
vuanphu <48764733+vuanphu@users.noreply.github.com>
2019-04-16 09:20:00 +0700

    V_c_iphone_asus

---
+++ ttn/WebBanDT/WebBanDT/Controllers/ApplePhoneController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models;
+
+namespace WebBanDT.Controllers
+{
+    public class ApplePhoneController : Controller
+    {
+        // GET: ApplePhone
+        public ActionResult Index()
+        {
+            ApplePhone strHome = new ApplePhone();
+
+            List<ApplePhone> obj = strHome.GetHome();
+            return View(obj);
+        }
+    }
+}
---
+++ ttn/WebBanDT/WebBanDT/Controllers/AsusPhoneController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models;
+
+namespace WebBanDT.Controllers
+{
+    public class AsusPhoneController : Controller
+    {
+        // GET: AsusPhone
+        public ActionResult Index()
+        {
+            AsusPhone strHome = new AsusPhone();
+
+            List<AsusPhone> obj = strHome.GetHome();
+            return View(obj);
+        }
+    }
+    }
