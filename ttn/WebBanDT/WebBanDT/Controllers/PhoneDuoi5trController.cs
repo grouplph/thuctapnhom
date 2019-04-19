d00335b68a1968199bf079bce8db5b89d9b778da
hieptran98 <hiephaha98@gmail.comm>
2019-04-19 09:20:08 +0700

    V_C_duoi5tr

---
+++ ttn/WebBanDT/WebBanDT/Controllers/PhoneDuoi5trController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models;
+
+namespace WebBanDT.Controllers
+{
+    public class PhoneDuoi5trController : Controller
+    {
+        // GET: PhoneDuoi5tr
+        public ActionResult Index()
+        {
+            PhoneDuoi5tr strHome = new PhoneDuoi5tr();
+
+            List<PhoneDuoi5tr> obj = strHome.GetHome();
+            return View(obj);
+        }
+    }
+}
