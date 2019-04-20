fe9c9a95f9086974f9c01579af4f0ea1f3bf9cc6
vuanphu <48764733+vuanphu@users.noreply.github.com>
2019-04-20 08:32:29 +0700

    v_c_sony_samsung

---
+++ ttn/WebBanDT/WebBanDT/Controllers/SamSungPhoneController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models;
+
+namespace WebBanDT.Controllers
+{
+    public class SamSungPhoneController : Controller
+    {
+        // GET: SamSungPhone
+        public ActionResult Index()
+        {
+            SamSungPhone strHome = new SamSungPhone();
+
+            List<SamSungPhone> obj = strHome.GetHome();
+            return View(obj);
+        }
+    }
+}
---
+++ ttn/WebBanDT/WebBanDT/Controllers/SonyPhoneController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models;
+
+namespace WebBanDT.Controllers
+{
+    public class SonyPhoneController : Controller
+    {
+        // GET: SonyPhone
+        public ActionResult Index()
+        {
+            SonyPhone strHome = new SonyPhone();
+
+            List<SonyPhone> obj = strHome.GetHome();
+            return View(obj);
+        }
+    }
+}
