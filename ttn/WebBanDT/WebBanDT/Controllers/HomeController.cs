7b57ba4452e0557e22fabc77235fcfa842721f50
LinhNguyen98CN <linhnguyen9806@gmail.com>
2019-04-20 14:45:25 +0700

    Thêm Tìm Kiếm

--- ttn/WebBanDT/WebBanDT/Controllers/HomeController.cs
+++ ttn/WebBanDT/WebBanDT/Controllers/HomeController.cs
@@ -12,12 +12,32 @@ namespace WebBanDT.Controllers
     {
         BanDT1 db = new BanDT1();
         // GET: Home
-        public ActionResult Index(int? pageTemp)
+        public ActionResult Index(int? pageTemp, string searchString)
         {
+            var phones = from l in db.Phones
+                         select l;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                phones = phones.Where(s => s.Name.Contains(searchString));
+            }
             int pageSize = 5;
             int pageNumber = pageTemp ?? 1;
-            return View(db.Phones.ToList().OrderBy(a => a.Id).ToPagedList(pageNumber, pageSize));
-            
+            return View(phones.ToList().OrderBy(a => a.Id).ToPagedList(pageNumber, pageSize));
+
+
+        }
+        [HttpGet]
+        public ActionResult Search(string searchString)
+        {
+            var phones = from l in db.Phones
+                         select l;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                phones = phones.Where(s => s.Name.Contains(searchString));
+
+            }
+            return View(phones);
+
         }
     }
 }
