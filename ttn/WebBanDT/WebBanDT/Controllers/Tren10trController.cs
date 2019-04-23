75d03422fd3348ffcf636a56406a5211a8833512
hieptran98 <hiephaha98@gmail.comm>
2019-04-23 21:30:31 +0700

    VC_tren10tr

---
+++ ttn/WebBanDT/WebBanDT/Controllers/Tren10trController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models;
+
+namespace WebBanDT.App_Start.Controllers
+{
+    public class Tren10trController : Controller
+    {
+        // GET: Tren10tr
+        public ActionResult Index()
+        {
+            Tren10tr strHome = new Tren10tr();
+            List<Tren10tr> obj = strHome.GetHome();
+            return View(obj);
+        }
+    }
+}
