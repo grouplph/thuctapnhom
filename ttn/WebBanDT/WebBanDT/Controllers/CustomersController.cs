cbb224389cedb7b0db10c3cb45ce677dec8e00b0
LinhNguyen98CN <linhnguyen9806@gmail.com>
2019-04-25 09:21:35 +0700

    Khách hàng Controller

---
+++ ttn/WebBanDT/WebBanDT/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models;
+using WebBanDT.Models.Data;
+using WebBanDT.Models.Function;
+
+namespace WebBanDT.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            var cart = (Customer)Session["CustomerSession"];
+            return View();
+        }
+        public ActionResult HoanThanh()
+        {
+            return View();
+        }
+    }
+}
