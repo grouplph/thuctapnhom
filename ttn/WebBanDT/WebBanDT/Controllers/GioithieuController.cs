c05fb6c2594260ae083f05ed47853f2efb2505f0
LinhNguyen98CN <linhnguyen9806@gmail.com>
2019-04-26 13:51:23 +0700

    ,

---
+++ ttn/WebBanDT/WebBanDT/Controllers/GioithieuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models.Data;
+
+namespace WebBanDT.App_Start.Controllers
+{
+    public class GioithieuController : Controller
+    {
+        // GET: Gioithieu
+        public ActionResult Index()
+        {
+            BanDT1 dt = new BanDT1();
+            return View();
+        }
+    }
+}
---
+++ ttn/WebBanDT/WebBanDT/Controllers/ManuFacturersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+using WebBanDT.Models.Data;
+
+namespace WebBanDT.Controllers
+{
+    public class ManuFacturersController : Controller
+    {
+        BanDT1 db = new BanDT1();
+        // GET: ManuFacturers
+        public ActionResult Index(int? pageTemp)
+        {
+            int pageSize = 10;
+            int pageNumber = pageTemp ?? 1;
+            return View(db.Manufacturers.ToList().OrderBy(a => a.Id).ToPagedList(pageNumber, pageSize));
+        }
+    }
+}
